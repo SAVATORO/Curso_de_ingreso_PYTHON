73ac9f90d712b47b89bd76f62ac6878071268084
FELIPE TORES ROJAS <138082693+SAVATORO@users.noreply.github.com>
2023-07-07 10:55:42 -0300

    nuevos cambios

--- ejercicios/01_entrada_salida/ejercicio_01/app_ES_01.py
+++ ejercicios/01_entrada_salida/ejercicio_01/app_ES_01.py
@@ -6,8 +6,8 @@ import customtkinter
 
 
 '''
-nombre:
-apellido:
+nombre:Brayam Felipe
+apellido:Torres Rojas
 ---
 Ejercicio: entrada_salida_01
 ---
@@ -29,7 +29,7 @@ class App(customtkinter.CTk):
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
-        pass
+        alert(title="ej1",message="Esto no anda, finciona")
 
 
 if __name__ == "__main__":
